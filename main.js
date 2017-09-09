9ed45a619908e2f81cfc1987ce651612ae9060b8
tanishi <tanishi503@gmail.com>
2017-09-09 17:13:59 +0900

    Add moment.js for operating date

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 const xray = require('x-ray');
+const moment = require('moment');
+
 const x = xray();
+
+moment.locale('ja');
 
 const userName = 'tanishi';
 const url = `https://github.com/users/${userName}/contributions`;
 
-x(url, 'rect', [ { count: '@data-count', date: '@data-date' }])((error, result) => {
+const xPromise = new Promise((resolve, reject) => {
+  x(url, 'rect', [ { count: '@data-count', date: '@data-date' }])((error, results) => {
     if (error) {
-        console.log(error);
+      console.log(error);
     } else {
-        console.log(result);
+      let continuity = 0;
+
+      results.map(result => {
+        const date = result.date;
+        const count = result.count;
+
+        if (moment(date).isSameOrAfter('2017-1-1')){
+          if (count > 0){
+            continuity++;
+          }
+          else {
+            continuity = 0;
+          }
+        }
+      });
+
+      console.log(continuity);
     }
+  });
 });
